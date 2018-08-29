0c6eed2d13f3df07255e17a41b22fceff6964843
Dmitriy Gavrilov <d_man_sun@mail.ru>
2018-08-29 09:38:19 +0600

    Table basic html,css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -17,6 +16,11 @@ export default {
 </script>
 
 <style lang="scss">
+  body {
+    margin: 0;
+    font-size: 16px;
+    line-height: 1.6;
+  }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,214 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="table">
+    <div class="table__inner">
+
+      <div class="table__filters">
+
+        <div class="table__filters-item">
+
+          <div class="table__filters-item-title">
+            Пол
+          </div>
+
+          <div class="table__filters-item-options-list">
+
+            <label for="checkbox-1" class="table__filters-item-option">
+
+              <input id="checkbox-1" type="checkbox" value="male">
+
+              <span class="table__filters-item-option-name">
+                  male (2)
+                </span>
+
+            </label>
+
+            <label for="checkbox-2" class="table__filters-item-option">
+
+              <input id="checkbox-2" type="checkbox" value="female">
+
+              <span class="table__filters-item-option-name">
+                  female (5)
+                </span>
+
+            </label>
+
+          </div>
+
+        </div>
+
+        <div class="table__filters-item">
+
+          <div class="table__filters-item-title">
+            Департамент
+          </div>
+
+          <div class="table__filters-item-options-list">
+
+            <label for="checkbox-3" class="table__filters-item-option">
+
+              <input id="checkbox-3" type="checkbox" value="Backend">
+
+              <span class="table__filters-item-option-name">
+                  Backend (2)
+                </span>
+
+            </label>
+
+            <label for="checkbox-4" class="table__filters-item-option">
+
+              <input id="checkbox-4" type="checkbox" value="Hr">
+
+              <span class="table__filters-item-option-name">
+                  Hr (1)
+                </span>
+
+            </label>
+
+            <label for="checkbox-5" class="table__filters-item-option">
+
+              <input id="checkbox-5" type="checkbox" value="Frontend">
+
+              <span class="table__filters-item-option-name">
+                  Frontend (1)
+                </span>
+
+            </label>
+
+          </div>
+
+        </div>
+
+        <div class="table__filters-item">
+
+          <div class="table__filters-item-title">
+            Город
+          </div>
+
+          <div class="table__filters-item-options-list">
+
+            <label for="checkbox-6" class="table__filters-item-option">
+
+              <input id="checkbox-6" type="checkbox" value="Moscow">
+
+              <span class="table__filters-item-option-name">
+                  Moscow (3)
+                </span>
+
+            </label>
+
+            <label for="checkbox-7" class="table__filters-item-option">
+
+              <input id="checkbox-7" type="checkbox" value="New-York">
+
+              <span class="table__filters-item-option-name">
+                  New-York (5)
+                </span>
+
+            </label>
+
+            <label for="checkbox-8" class="table__filters-item-option">
+
+              <input id="checkbox-8" type="checkbox" value="Voronezh">
+
+              <span class="table__filters-item-option-name">
+                  Voronezh (2)
+                </span>
+
+            </label>
+
+            <label for="checkbox-9" class="table__filters-item-option">
+
+              <input id="checkbox-9" type="checkbox" value="Spb">
+
+              <span class="table__filters-item-option-name">
+                  Spb (1)
+                </span>
+
+            </label>
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <div class="table__rows">
+
+        <div class="table__row">
+
+          <div class="table__td">
+            Имя
+          </div>
+
+          <div class="table__td">
+            Возраст
+          </div>
+
+          <div class="table__td">
+            Пол
+          </div>
+
+          <div class="table__td">
+            Департамент
+          </div>
+
+          <div class="table__td">
+            Адрес
+          </div>
+
+        </div>
+
+        <div class="table__row">
+
+          <div class="table__td">
+            Mendez
+          </div>
+
+          <div class="table__td">
+            30
+          </div>
+
+          <div class="table__td">
+            male
+          </div>
+
+          <div class="table__td">
+            department
+          </div>
+
+          <div class="table__td">
+            Moscow, Fayette Street 923
+          </div>
+
+        </div>
+
+        <div class="table__row">
+
+          <div class="table__td">
+            Sharon
+          </div>
+
+          <div class="table__td">
+            24
+          </div>
+
+          <div class="table__td">
+            female
+          </div>
+
+          <div class="table__td">
+            Backend
+          </div>
+
+          <div class="table__td">
+            Moscow, Brighton Avenue 666
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -41,18 +223,98 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+  $mobile-width : 899px;
+
+  .table {
+    display: block;
+    max-width: 1200px;
+    margin: 50px auto;
+    border: 1px solid black;
+
+    @media (max-width:$mobile-width) {
+      max-width: 100%;
+    }
+
+  }
+
+  .table__inner {
+    display: block;
+    position: relative;
+
+    @media (max-width:$mobile-width) {
+      display: flex;
+      flex-direction: row;
+      align-items: stretch;
+      justify-content: space-between;
+    }
+
+  }
+
+  // Filters
+
+  .table__filters {
+    display: flex;
+    align-items: stretch;
+    flex-direction: row;
+    justify-content: start;
+    border: 1px solid black;
+    border-width: 0 0 1px 0;
+    background-color: rgba(134, 134, 134, 0.48);
+
+    @media (max-width:$mobile-width) {
+      border-width: 0 1px 0 0;
+      width: 120px;
+      min-width: 120px;
+      flex-wrap: wrap;
+    }
+
+    &-item {
+      display: block;
+      width: 33.3%;
+      border: 1px solid black;
+      border-width: 0 1px 0 0;
+      padding: 10px;
+      word-break: break-word;
+
+      &:last-child {
+        border-width: 0;
+
+        @media (max-width:$mobile-width) {
+          border-width: 0 0 1px 0;
+        }
+
+      }
+
+      @media (max-width:$mobile-width) {
+        width: 100%;
+        border-width: 0 0 1px 0;
+      }
+
+    }
+
+    &-item-title {
+      padding-bottom: 10px;
+      font-weight: bold;
+    }
+
+    &-item-option {
+      display: block;
+      text-align: left;
+      cursor: pointer;
+    }
+
+  }
+
+  // Rows
+
+  .table__rows {
+    display: block;
+    width: 100%;
+  }
+
+
+
+
+
 </style>
